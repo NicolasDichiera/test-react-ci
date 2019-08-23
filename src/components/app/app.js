52d899c3fc5f4fefd032d87cf44216f65fccf6be
Hernan Tomassini <hernan.tomassini@hotmail.com>
2019-08-23 17:46:02 -0300

    Header and Drawer

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
 import CssBaseline from '@material-ui/core/CssBaseline'
+import Container from '@material-ui/core/Container'
+
+import HeaderBar from '../header/header'
+import ResponsiveDrawer from '../drawer/drawer'
 
 function App () {
   return (
     <>
       <CssBaseline />
-      <Button variant='contained' color='primary'>
-        Hello World
-      </Button>
+      <HeaderBar />
+      <ResponsiveDrawer />
+
+      <Container maxWidth='md'>
+        <Button variant='contained' color='primary'>
+          Hello World
+        </Button>
+      </Container>
+
     </>
   )
 }
