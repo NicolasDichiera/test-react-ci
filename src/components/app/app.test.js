dfee795f337b4df5dee9cbfbe877d5f085d45bc8
Hernan Tomassini <hernan.tomassini@hotmail.com>
2019-08-23 16:17:39 -0300

    Test Travis - Fix tests

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -3,11 +3,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
-import { fail } from 'assert'
 
 it('renders without crashing', () => {
-  fail()
-  // const div = document.createElement('div')
-  // ReactDOM.render(<App />, div)
-  // ReactDOM.unmountComponentAtNode(div)
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+  ReactDOM.unmountComponentAtNode(div)
 })
